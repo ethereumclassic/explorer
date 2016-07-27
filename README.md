bae7884adbc0f4e86dac081531e704d80fa14f8f
Elaine Ou <elaine.ou@gmail.com>
2016-07-27 10:34:56 -0700

    copy from aakilfernandes

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
-# explorer
-EthereumClassic Block Explorer
+# EthExplorer (In Progress)
+
+![EthExplorer Screenshot](http://i.imgur.com/NHFYq0x.png)
+
+##License
+
+The code in this branch is licensed under GPLv3 (see LICENSE file)
+
+Code in the MIT branch is under the MIT License (shocker, amirite)
+
+But seriously, license file has a TL;DR, at least look at that before using this code in a project
+
+##Installation
+
+Install [Git](https://git-scm.com/book/en/v2/Getting-Started-Installing-Git "Git installation") if you haven't already
+
+Clone the repo
+
+`git clone https://github.com/etherparty/explorer`
+
+Download [Nodejs and npm](https://docs.npmjs.com/getting-started/installing-node "Nodejs install") if you don't have them
+
+Start the program. All dependencies will be automatically downloaded
+
+`npm start`
+
+Then visit http://localhost:8000 in your browser of choice. You might get an error message:
+
+`geth --rpc --rpccorsdomain "http://localhost:8000"`
+
+Install [geth](https://github.com/ethereum/go-ethereum/wiki/Building-Ethereum "Geth install") if you don't already have it, then run the above command.
+
+Then refresh the page in your browser 
---
+++ app/app.js
@@ -0,0 +1,42 @@
+'use strict';
+
+angular.module('ethExplorer', ['ngRoute','ui.bootstrap'])
+
+.config(['$routeProvider',
+    function($routeProvider) {
+        $routeProvider.
+            when('/', {
+                templateUrl: 'views/main.html',
+                controller: 'mainCtrl'
+            }).
+            when('/block/:blockId', {
+                templateUrl: 'views/blockInfos.html',
+                controller: 'blockInfosCtrl'
+            }).
+            when('/transaction/:transactionId', {
+                templateUrl: 'views/transactionInfos.html',
+                controller: 'transactionInfosCtrl'
+            }).
+            when('/address/:addressId', {
+                templateUrl: 'views/addressInfos.html',
+                controller: 'addressInfosCtrl'
+            }).
+            otherwise({
+                redirectTo: '/'
+            });
+    }])
+    .run(function($rootScope) {
+        var web3 = require('web3');
+        var eth_node_url = "http://192.241.148.241:8546";
+        web3.setProvider(new web3.providers.HttpProvider(eth_node_url));
+        $rootScope.web3=web3;
+        function sleepFor( sleepDuration ){
+            var now = new Date().getTime();
+            while(new Date().getTime() < now + sleepDuration){ /* do nothing */ } 
+        }
+        var connected = false;
+        if(!web3.isConnected()) {
+            $('#connectwarning').modal({keyboard:false,backdrop:'static'}) 
+            $('#connectwarning').modal('show') 
+        }
+    });
