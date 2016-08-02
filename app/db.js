facb2b836ffcde00e2f386b664ad9187354ed0b9
Elaine Ou <elaine.ou@gmail.com>
2016-08-02 10:44:37 -0700

    add db structure for mongodb

---
+++ app/db.js
@@ -0,0 +1,44 @@
+var mongoose = require( 'mongoose' );
+var Schema   = mongoose.Schema;
+
+var Block = new Schema(
+{
+    "number": Number,
+    "hash": String,
+    "parentHash": String,
+    "nonce": String,
+    "sha3Uncles": String,
+    "logsBloom": String,
+    "transactionsRoot": String,
+    "stateRoot": String,
+    "receiptRoot": String,
+    "miner": String,
+    "difficulty": String,
+    "totalDifficulty": String,
+    "size": Number,
+    "extraData": String,
+    "gasLimit": Number,
+    "gasUsed": Number,
+    "timestamp": Number,
+    "transactions": [
+        {
+            "hash": String,
+            "nonce": Number,
+            "blockHash": String,
+            "blockNumber": Number,
+            "transactionIndex": Number,
+            "from": String,
+            "to": String,
+            "value": String,
+            "gas": Number,
+            "gasPrice": String,
+            "input": String
+        }
+    ],
+    "uncles": [{"hash": String}]
+});
+
+var blockDB = mongoose.model('Block', Block);
+module.exports.Block = mongoose.model('Block');
+
+mongoose.connect( 'mongodb://localhost/blockDB' );
