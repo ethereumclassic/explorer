1de9bcf70017dca9f7519f0f26a9e70a8c33a0e1
Elaine Ou <elaine.ou@gmail.com>
2016-08-03 22:17:07 -0700

    basic front end

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ require( './db' );
 
 var express = require('express');
 var path = require('path');
-//var favicon = require('serve-favicon');
+var favicon = require('serve-favicon');
 var logger = require('morgan');
 var bodyParser = require('body-parser');
 var routes = require('./routes');
@@ -16,7 +16,7 @@ app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-//app.use(favicon(__dirname + '/public/favicon.ico'));
+app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
