e93841a6f62540d15eaf752983a076f1289f5c4d
Elaine Ou <elaine.ou@gmail.com>
2016-08-04 22:48:49 -0700

    datatables look pretty good

--- public/js/controllers/AddressController.js
+++ public/js/controllers/AddressController.js
@@ -15,19 +15,35 @@ angular.module('BlocksApp').controller('AddressController', function($stateParam
       url: URL,
       data: {"addr": $scope.addrHash}
     }).success(function(data) {
-      //$scope.transactions = data;
-      console.log(data)
-      //TableAjax.handleRecords(data);
       $("#table_txs").DataTable({
         "data": data,
         "lengthMenu": [
                     [10, 20, 50, 100, 150, -1],
                     [10, 20, 50, 100, 150, "All"] // change per page values here
                 ],
-                "pageLength": 10, // default record count per page
-                "order": [
-                    [1, "asc"]
-                ] // set first column as a default sort by asc
+        "pageLength": 10, 
+        "order": [
+            [6, "desc"]
+        ],
+        "language": {
+          "lengthMenu": "_MENU_ transactions",
+          "zeroRecords": "No transactions found",
+          "infoEmpty": "No transactionss available",
+          "infoFiltered": "(filtered from _MAX_ total txs)"
+        },
+        "columnDefs": [ 
+          {"type": "date", "targets": 6},
+          {"orderable": false, "targets": [0,2,3]},
+          { "render": function(data, type, row) {
+                        if (data != $scope.addrHash)
+                          return '<a href="/addr/'+data+'">'+data+'</a>'
+                        else
+                          return data
+                      }, "targets": [0,2,3]},
+          { "render": function(data, type, row) {
+                        return '<a href="/block/'+data+'">'+data+'</a>'
+                      }, "targets": [1]}
+          ]
       })
     });
 
