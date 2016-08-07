c651d0e7e410ce3ae53842906bfff191a171e4ab
Cody Burns <cody.w.burns@gmail.com>
2016-08-07 07:51:48 -0500

    add unit conversion
    etherunits.js corrects the addr.balance from wei to normal units and limits decimal places

--- public/js/controllers/AddressController.js
+++ public/js/controllers/AddressController.js
@@ -19,6 +19,9 @@ angular.module('BlocksApp').controller('AddressController', function($stateParam
         $rootScope.$state.current.data["pageTitle"] = "Contract Address";
     });
 
+    // fix units
+    $scope.addr = etherUnits.toEther(addr.balance, 'wei');
+
     //fetch transactions
     $http({
       method: 'POST',
