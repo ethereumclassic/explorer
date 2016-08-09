6df7501790fcd5e19946f97358dca7ba1f052288
Elaine Ou <elaine.ou@gmail.com>
2016-08-08 17:51:42 -0700

    add summary stats and controller

--- public/js/controllers/HomeController.js
+++ public/js/controllers/HomeController.js
@@ -6,6 +6,7 @@ angular.module('BlocksApp').controller('HomeController', function($rootScope, $s
 
     var URL = '/data';
 
+    $rootScope.isHome = true;
 
     $scope.reloadBlocks = function() {
       $scope.blockLoading = true;
@@ -37,9 +38,37 @@ angular.module('BlocksApp').controller('HomeController', function($rootScope, $s
     $scope.txLoading = false;
     $scope.blockLoading = false;
 })
+.directive('summaryStats', function($http) {
+  return {
+    restrict: 'E',
+    templateUrl: '/views/summary-stats.html',
+    scope: true,
+    link: function(scope, elem, attrs){
+      scope.stats = {};
+        //fetch stats stuff 
+        // TODO (Elaine): use our own API
+        var statsURL = "http://cors.io/?u=http://ec2-52-42-175-9.us-west-2.compute.amazonaws.com/api/eth.json";
+        $http.get(statsURL)
+         .then(function(res){
+            console.log(res.data)
+            scope.stats = {
+              "usdEtc": res.data[0].substr(1,res.data[0].length),
+              "usdEth": res.data[10].substr(1,res.data[0].length),
+              "etcHashrate": res.data[9]
+            };
+            scope.stats.usdEtcEth = parseInt(100*parseFloat(scope.stats.usdEtc)/parseFloat(scope.stats.usdEth));
+            scope.stats.etcEthHash = parseInt(parseFloat(res.data[9])/(10*parseFloat(res.data[19])));
+            scope.stats.ethChange = parseFloat(res.data[13].substr(2, res.data[13].length))
+          });
+          
+        }
+  }
+})
 
-angular.module('BlocksApp').filter('timeDuration', function() {
+
+angular.module('BlocksApp')
+.filter('timeDuration', function() {
   return function(timestamp) {
     return getDuration(timestamp).toString();
   };
-});
+})
