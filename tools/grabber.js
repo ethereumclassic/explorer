27c426bdb30c521e6d9c35abc37334fb722c08d0
Elaine Ou <elaine.ou@gmail.com>
2016-08-14 15:33:56 -0700

    get block numbr

--- tools/grabber.js
+++ tools/grabber.js
@@ -166,7 +166,7 @@ var patchBlocks = function(config) {
 
     // number of blocks should equal difference in block numbers
     var firstBlock = 0;
-    var lastBlock = web3.eth.getBlock("latest").blockNumber;
+    var lastBlock = web3.eth.getBlock("latest").number;
     blockIter(web3, firstBlock, lastBlock, config);
 }
 
