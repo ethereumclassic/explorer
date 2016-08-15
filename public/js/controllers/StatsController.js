b2d592e0fa56902781f556124e6a78d4aa10ddc4
Elaine Ou <elaine.ou@gmail.com>
2016-08-15 10:35:15 -0700

    put title back on chart

--- public/js/controllers/StatsController.js
+++ public/js/controllers/StatsController.js
@@ -1,5 +1,6 @@
 angular.module('BlocksApp').controller('StatsController', function($stateParams, $rootScope, $scope) {
 
+    $rootScope.isHome = false;
     $rootScope.$state.current.data["pageTitle"] = "ETC Hashrate Growth"
     
 })
