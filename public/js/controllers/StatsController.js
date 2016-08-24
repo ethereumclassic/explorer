2acacda25204527b03c04774fd8d4d81bb10018e
Elaine Ou <elaine.ou@gmail.com>
2016-08-24 16:01:17 -0700

    accommodate different charts

--- public/js/controllers/StatsController.js
+++ public/js/controllers/StatsController.js
@@ -1,6 +1,22 @@
 angular.module('BlocksApp').controller('StatsController', function($stateParams, $rootScope, $scope) {
 
     $rootScope.isHome = false;
-    $rootScope.$state.current.data["pageTitle"] = "ETC Hashrate Growth"
-    
+  
+    /*
+      Chart types: 
+        etc_hashrate: ETC Hashrate Growth
+        miner_hashrate: Miner Hashrate Distribution
+    */
+
+    const CHART_TYPES = {
+        "etc_hashrate": {
+            "title": "ETC Hashrate Growth"
+        },
+        "miner_hashrate": {
+            "title": "Miner Hashrate Distribution"
+        }
+    }
+
+    $rootScope.$state.current.data["pageSubTitle"] = CHART_TYPES[$stateParams.chart].title;
+
 })
