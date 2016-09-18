b1f49f7bda6d1a9f5e3a645ec680ef962199c9bb
Elaine Ou <elaine.ou@gmail.com>
2016-09-18 10:01:04 -0700

    retrieve txs

--- tools/fix_tx.js
+++ tools/fix_tx.js
@@ -4,46 +4,44 @@
 
 var mongoose = require( 'mongoose' );
 require( '../db.js' );
+var async = require('async');
 var Block     = mongoose.model( 'Block' );
 var Transaction     = mongoose.model( 'Transaction' );
 
 var getTx = function(collection) {
   mongoose.connection.on("open", function(err,conn) { 
 
-    var bulkOps = [];
-    var count = 0;
-    var missingCount = 5200;
-
-    collection.find().forEach(function(doc) {
-      if (doc.transactions.length > 0) {
-      setTimeout(function() {
-        for (d in doc.transactions) {
-            var txData = doc.transactions[d];
-            txData.timestamp = doc.timestamp;
-            bulkOps.push(txData);
-        }
-          Transaction.collection.insert(bulkOps, {upsert: true}, function( err, tx ){
-            if ( typeof err !== 'undefined' && err ) {
-                if (err.code == 11000) {
-                    console.log('Skip: Duplicate key ' + 
-                    tx.hash + ': ' + 
-                    err);
-                } else {
-                   console.log('Error: Aborted due to error: ' + 
-                        err);
-                   process.exit(9);
-               }
-            } else {
-                console.log('DB successfully written for block ' +
-                    doc.number.toString() );
+    Block.find({}, "transactions timestamp").lean(true).exec(function(err, docs) {
+        for (b in docs) {
+            var doc = docs[b];
+            var bulkOps = [];
+          if (doc.transactions.length > 0) {
+            for (d in doc.transactions) {
+                var txData = doc.transactions[d];
+                txData.timestamp = doc.timestamp;
+                bulkOps.push(txData);
             }
-            bulkOps = [];
-          });
-    
-      }, 1000);
-      }
-    });
+              Transaction.collection.insert(bulkOps, function( err, tx ){
+                if ( typeof err !== 'undefined' && err ) {
+                    if (err.code == 11000) {
+                        console.log('Skip: Duplicate key ' + 
+                        err);
+                    } else {
+                       console.log('Error: Aborted due to error: ' + 
+                            err);
+                       process.exit(9);
+                   }
+                } else {
+                    console.log('DB successfully written for block ' +
+                        tx.length.toString() );
+                    
+                }
+                bulkOps = [];
+              });
         
+          }
+    }
+      });  
   })
 }
 
