f9f219035fce6654c59554bfe9b5d8f63c193e9d
Elaine Ou <elaine.ou@gmail.com>
2016-09-17 17:14:53 -0700

    display internal txs

--- public/js/controllers/AddressController.js
+++ public/js/controllers/AddressController.js
@@ -21,6 +21,7 @@ angular.module('BlocksApp').controller('AddressController', function($stateParam
       $scope.addr = data;
       if (data.isContract) {
         $rootScope.$state.current.data["pageTitle"] = "Contract Address";
+        fetchInternalTxs();
       }
     });
 
@@ -80,7 +81,52 @@ angular.module('BlocksApp').controller('AddressController', function($stateParam
       $("#table_wait").remove();
     });
 
-
+    var fetchInternalTxs = function() {
+      $http({
+        method: 'POST',
+        url: '/internal',
+        data: {"addr": $scope.addrHash}
+      }).success(function(data) {
+        $("#table_internal_txs").DataTable({
+          "data": data,
+          "lengthMenu": [
+                      [10, 20, 50, 100, 150, -1],
+                      [10, 20, 50, 100, 150, "All"] // change per page values here
+                  ],
+          "pageLength": 10, 
+          "order": [
+              [6, "desc"]
+          ],
+          "language": {
+            "lengthMenu": "_MENU_ transactions",
+            "zeroRecords": "No transactions found",
+            "infoEmpty": ":(",
+            "infoFiltered": "(filtered from _MAX_ total txs)"
+          },
+          "columnDefs": [ 
+            { "targets": [ 5 ], "visible": false, "searchable": false },
+            {"type": "date", "targets": 6},
+            {"orderable": false, "targets": [0,2,3]},
+            { "render": function(data, type, row) {
+                          if (data != $scope.addrHash)
+                            return '<a href="/addr/'+data+'">'+data+'</a>'
+                          else
+                            return data
+                        }, "targets": [2,3]},
+            { "render": function(data, type, row) {
+                          return '<a href="/block/'+data+'">'+data+'</a>'
+                        }, "targets": [1]},
+            { "render": function(data, type, row) {
+                          return '<a href="/tx/'+data+'">'+data+'</a>'
+                        }, "targets": [0]},
+            { "render": function(data, type, row) {
+                          return getDuration(data).toString();
+                        }, "targets": [6]},
+            ]
+        });
+        $("#table_int_wait").remove();
+      });
+    }
 })
 .directive('contractSource', function($http) {
   return {
