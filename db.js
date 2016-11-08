00be3e301fe6dfaaff153c2b80f474f993e724b5
Elaine Ou <elaine.ou@gmail.com>
2016-11-07 17:01:12 -0800

    Transactions deprecated for InternalTx

--- db.js
+++ db.js
@@ -35,28 +35,10 @@ var Contract = new Schema(
     "byteCode": String
 }, {collection: "Contract"});
 
-var Transaction = new Schema(
-{
-    "hash": {type: String, index: {unique: true}},
-    "nonce": Number,
-    "blockHash": String,
-    "blockNumber": Number,
-    "transactionIndex": Number,
-    "from": String,
-    "to": String,
-    "value": String,
-    "gas": Number,
-    "gasPrice": String,
-    "timestamp": Number,
-    "input": String
-});
-
 mongoose.model('Block', Block);
 mongoose.model('Contract', Contract);
-mongoose.model('Transaction', Transaction);
 module.exports.Block = mongoose.model('Block');
 module.exports.Contract = mongoose.model('Contract');
-module.exports.Transaction = mongoose.model('Transaction');
 
 mongoose.connect( 'mongodb://localhost/blockDB' );
 mongoose.set('debug', true);
