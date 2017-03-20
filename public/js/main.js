5b8150ce755e916d6c38fb8af1278e7c32e014f7
Elaine Ou <elaine.ou@gmail.com>
2017-03-20 13:28:16 -0700

    update token links

--- public/js/main.js
+++ public/js/main.js
@@ -226,6 +226,24 @@ BlocksApp.config(['$stateProvider', '$urlRouterProvider', function($stateProvide
             }
         })
 
+        .state('token', {
+            url: "/token/{hash}",
+            templateUrl: "views/token.html",
+            data: {pageTitle: 'Tokens'},
+            controller: "TokenController",
+            resolve: {
+                deps: ['$ocLazyLoad', function($ocLazyLoad) {
+                    return $ocLazyLoad.load({
+                        name: 'BlocksApp',
+                        insertBefore: '#ng_load_plugins_before', 
+                        files: [
+                             '/js/controllers/TokenController.js'
+                        ]
+                    });
+                }]
+            }
+        })
+
         .state('dao', {
             url: "/dao",
             templateUrl: "views/dao.html",
