cdaa28b3520195b543bf4e1a446d5f601bfee90d
elaineo <elaine.ou@gmail.com>
2018-01-05 20:26:53 -0800

    fix parsing of number

--- public/js/controllers/HomeController.js
+++ public/js/controllers/HomeController.js
@@ -66,12 +66,12 @@ angular.module('BlocksApp').controller('HomeController', function($rootScope, $s
         });
       $http.get(etcPriceURL)
        .then(function(res){
-          scope.stats.usdEtc = res.data[0]["price_usd"].toFixed(2);
+          scope.stats.usdEtc = parseFloat(res.data[0]["price_usd"]);
           scope.stats.usdEtcEth = parseInt(100*scope.stats.usdEtc/scope.stats.usdEth);
         });
       $http.get(ethPriceURL)
        .then(function(res){
-          scope.stats.usdEth = res.data[0]["price_usd"].toFixed(2);
+          scope.stats.usdEth = parseFloat(res.data[0]["price_usd"];
           scope.stats.usdEtcEth = parseInt(100*scope.stats.usdEtc/scope.stats.usdEth);
           scope.stats.ethChange = parseFloat(res.data.change);
         });
