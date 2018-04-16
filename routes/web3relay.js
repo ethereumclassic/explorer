4049a3a1fe7b48f766f014089ca14136afe9c5f3
GeoSky Alert <mike@geoskygps.com>
2018-04-16 18:26:33 -0500

    web3 switched to mordenapi

--- routes/web3relay.js
+++ routes/web3relay.js
@@ -15,7 +15,7 @@ var filterTrace = require('./filters').filterTrace;
 if (typeof web3 !== "undefined") {
   web3 = new Web3(web3.currentProvider);
 } else {
-  web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
+  web3 = new Web3(new Web3.providers.HttpProvider("http://mordenexplorer.ethertrack.io:8545"));
 }
 
 if (web3.isConnected())
