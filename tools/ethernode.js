f9b1cbb436b81e57af809e98c849ba4b9453aeae
Burns, Cody <cody.burns@accenture.com>
2018-08-31 13:31:56 -0500

    fix username and passwd
    fix user name and password

--- tools/ethernode.js
+++ tools/ethernode.js
@@ -1,5 +1,5 @@
 //* Web3 information *//
-var Web3 = require('web3');
+var Web3 = require('zsl-web3.js');
 var fs = require('fs');
 
 /**
@@ -39,6 +39,7 @@ function buildURI(config) {
   }
   //true is using unsigned certs
   if (!('unsafeTLS' in config) || (config.unsafeTLS)) {
+    console.log("unsafely ignoring self signed certs!");
     process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
   }
   //timeout
@@ -50,16 +51,16 @@ function buildURI(config) {
     var uri = protocol
               + config.nodeAddr + ":" + config.gethPort.toString()
               + "," + config.timeout.toString();
-    console.log('Connecting ' + config.nodeAddr + ':' + config.gethPort + '...');
+    //console.log('Connecting ' + protocol  + config.nodeAddr + ':' + config.gethPort + '...');
   } else {
-    var uri = protocol
+    var uri = {"url" : protocol
             + config.nodeAddr + ":" + config.gethPort.toString()
-            + "," + config.timeout.toString()
-            + "," + config.userName
-            + "," + config.password;
-   console.log('Connecting ' + config.nodeAddr + ':' + config.gethPort + ' with user: ' + config.userName + ' ...');
+            , "timeout" : config.timeout.toString()
+            , "userName" : config.userName
+            , "password" : config.password};
+   //console.log('Connecting ' + protocol + config.nodeAddr + ':' + config.gethPort + ' with user: ' + config.userName + ' ...');
   }
   return uri;
 }
 
-module.exports = new Web3(new Web3.providers.HttpProvider(buildURI(config)));
+module.exports = new Web3(new Web3.providers.HttpProvider(buildURI(config).url, buildURI(config).timeout, buildURI(config).userName, buildURI(config).password));
