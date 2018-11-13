ad2da4949194cc235dfdceebdb5c02d34c9fd244
dontpanic <cody.w.burns@gmail.com>
2018-11-13 20:45:54 +0000

    updated test folder structure

---
+++ test/index.js
@@ -0,0 +1,14 @@
+var assert = require('assert');
+
+
+//* load unit tests *//
+
+var app = require('./units/apps');
+
+//* end unit tests *//
+
+/* tests to run (add tests here)  */
+
+describe('Global functions', function() {
+	app
+});
