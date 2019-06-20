7fcf82aafd8e691cadcee2c7627f7e8b0693d0d0
Vanpav <vanpav@gmail.com>
2019-06-20 12:41:12 +0300

    Auto update mainpage data. Tokens removed

--- public/js/controllers/HomeController.js
+++ public/js/controllers/HomeController.js
@@ -1,63 +1,100 @@
-angular.module('BlocksApp').controller('HomeController', function($rootScope, $scope, $http, $timeout) {
-    $scope.$on('$viewContentLoaded', function() {
-        // initialize core components
-        App.initAjax();
+var FETCH_DELAY = 3000;
+
+angular.module('BlocksApp')
+  .controller('HomeController', function ($rootScope, $scope, $http, $timeout) {
+    $scope.$on('$viewContentLoaded', function () {
+      // initialize core components
+      App.initAjax();
     });
-
+    
     var URL = '/data';
-
+    var timeout;
+    
     $rootScope.isHome = true;
-
-    $scope.reloadBlocks = function() {
+    
+    $scope.reloadBlocks = function () {
       $scope.blockLoading = true;
-      $http({
+      return $http({
         method: 'POST',
         url: URL,
-        data: {"action": "latest_blocks"}
-      }).then(function(resp) {
-        $scope.latest_blocks = resp.data.blocks;
-        $scope.blockLoading = false;
-      });
-    }
-    $scope.reloadTransactions = function() {
+        data: { 'action': 'latest_blocks' },
+      })
+        .then(function (resp) {
+          $scope.latest_blocks = resp.data.blocks;
+          $scope.blockLoading = false;
+        });
+    };
+    
+    $scope.reloadTransactions = function () {
       $scope.txLoading = true;
-      $http({
+      return $http({
         method: 'POST',
         url: URL,
-        data: {"action": "latest_txs"}
-      }).then(function(resp) {
-        $scope.latest_txs = resp.data.txs;
-        $scope.txLoading = false;
-      });
+        data: { 'action': 'latest_txs' },
+      })
+        .then(function (resp) {
+          $scope.latest_txs = resp.data.txs;
+          $scope.txLoading = false;
+        });
+    };
+  
+    function reloadBoth() {
+      return Promise.all([$scope.reloadBlocks(), $scope.reloadTransactions()]);
     }
-    $scope.reloadBlocks();
-    $scope.reloadTransactions();
+    function startTimeout() {
+      cancelTimeout();
+      reloadBoth().then(
+        timeout = $timeout(startTimeout, FETCH_DELAY)
+      );
+    }
+    function cancelTimeout() {
+      $timeout.cancel(timeout);
+      timeout = undefined;
+    }
+    
+    startTimeout();
     $scope.txLoading = false;
     $scope.blockLoading = false;
     $scope.settings = $rootScope.setup;
-})
-.directive('simpleSummaryStats', function($http) {
-  return {
-    restrict: 'E',
-    templateUrl: '/views/simple-summary-stats.html',
-    scope: true,
-    link: function(scope, elem, attrs){
-      scope.stats = {};
-      var statsURL = "/web3relay";
-      $http.post(statsURL, {"action": "hashrate"})
-       .then(function(res){
-          scope.stats.hashrate = res.data.hashrate;
-          scope.stats.difficulty = res.data.difficulty;
-          scope.stats.blockHeight = res.data.blockHeight;
-          scope.stats.blockTime = res.data.blockTime;
-          //console.log(res);
-	});
-      }
-  }
-})
-.directive('siteNotes', function() {
-  return {
-    restrict: 'E',
-    templateUrl: '/views/site-notes.html'
-  }
-});
+    $scope.$on('$destroy', cancelTimeout);
+  })
+  .directive('simpleSummaryStats', function ($http, $timeout) {
+    return {
+      restrict: 'E',
+      templateUrl: '/views/simple-summary-stats.html',
+      scope: true,
+      link: function (scope, elem, attrs) {
+        scope.stats = {};
+        var statsURL = '/web3relay';
+        var timeout;
+        function getData() {
+          return $http.post(statsURL, { 'action': 'hashrate' })
+            .then(function (res) {
+              // scope.stats.hashrate = res.data.hashrate;
+              // scope.stats.difficulty = res.data.difficulty;
+              scope.stats.blockHeight = res.data.blockHeight;
+              scope.stats.blockTime = res.data.blockTime;
+              //console.log(res);
+            });
+        }
+        function startTimeout() {
+          getData().then(function() {
+            timeout = $timeout(startTimeout, FETCH_DELAY);
+          });
+        }
+        
+        startTimeout();
+        scope.$on('$destroy', function() {
+          console.log('destroy');
+          $timeout.cancel(timeout);
+          timeout = undefined;
+        })
+      },
+    };
+  })
+  .directive('siteNotes', function () {
+    return {
+      restrict: 'E',
+      templateUrl: '/views/site-notes.html',
+    };
+  });
